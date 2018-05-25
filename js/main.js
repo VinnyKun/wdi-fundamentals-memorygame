afd3ecd93098e71b86b91c9e1c153661f30757a8
vinnykun <vineethkunnathsg@gmail.com>
2018-05-25 14:42:34 +0800

    created game logic

---
+++ js/main.js
@@ -0,0 +1,10 @@
+console.log("Up and running!");
+console.log("how bow now");
+
+var cardOne = 'queen';
+var cardTwo = 'queen';
+var cardThree = 'king';
+var cardFour = 'king';
+
+console.log("user flipped " + cardThree);
+console.log("user flipped " + cardOne);
