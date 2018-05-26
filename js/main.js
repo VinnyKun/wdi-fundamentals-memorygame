0379d7315f3314ac77c7c3ed2c3744ca62468553
vinnykun <vineethkunnathsg@gmail.com>
2018-05-26 16:25:19 +0800

    improved game logic with functions

--- js/main.js
+++ js/main.js
@@ -1,23 +1,30 @@
 console.log("Up and running!");
 console.log("how bow now");
 
+// global arrays
 var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
+//check for match function
+var checkForMatch = function(){
 
-cardsInPlay.push(cardOne);
-console.log("user flipped " + cardOne);
-
-var cardTwo = cards[3];
-
-cardsInPlay.push(cardTwo);
-console.log("user flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1])
 		alert("You found a match!")
 	else
 		alert("sorry, try again")
+};
+
+// game logic function
+var flipCard = function (cardId) {
+
+console.log("user flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+
+if (cardsInPlay.length === 2) {
+	checkForMatch();
 }
+};
+
+flipCard(0);
+flipCard(2);
