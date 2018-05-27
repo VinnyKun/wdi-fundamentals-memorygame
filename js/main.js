eebc7a61a46c315e7ba136482b009d632e0d738f
vinnykun <vineethkunnathsg@gmail.com>
2018-05-27 17:36:54 +0800

    after dom module, cards be flippin

--- js/main.js
+++ js/main.js
@@ -28,6 +28,7 @@ var cards = [
 
 var cardsInPlay = [];
 
+
 //check for match function
 var checkForMatch = function(){
 
@@ -38,13 +39,16 @@ var checkForMatch = function(){
 };
 
 // game logic function
-var flipCard = function (cardId) {
+var flipCard = function () {
+
+var cardId = this.getAttribute('data-id');
 
 console.log("user flipped " + cards[cardId].rank);
 cardsInPlay.push(cards[cardId].rank);
 console.log(cards[cardId].cardImage)
 console.log(cards[cardId].suit)
 
+this.setAttribute('src', cards[cardId].cardImage);
 
 if (cardsInPlay.length === 2) {
 	checkForMatch();
@@ -52,8 +56,17 @@ if (cardsInPlay.length === 2) {
 
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+	for (var i = 0; i < 4 ; i += 1 ){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);	
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard ();
 
 
 
